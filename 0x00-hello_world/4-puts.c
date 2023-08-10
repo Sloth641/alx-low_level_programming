fea9757608c6538b166f153006c6c70d88e82150
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-10 09:58:16 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Description: a C program that prints a literal string using puts function
+ *
+ *Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
