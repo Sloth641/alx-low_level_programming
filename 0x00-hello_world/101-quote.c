8871a7ff153abb9e57acab80578a8f529e016334
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-10 10:59:27 +0000

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Description: prints the size of various types on different computers
+ *
+ *Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	fprintf(stderr,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
