ca9ac2545ef16f4a37bd8836fc99afaad665bb35
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-10 10:06:39 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Description: a C program that prints a literal string using printf function
+ *
+ *Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
