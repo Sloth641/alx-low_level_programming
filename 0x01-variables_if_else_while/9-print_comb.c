fb64e2a2525fe2f14a655ae68ecaf1572425caf3
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-11 22:21:23 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *Description: a C program that prints numbers
+ *
+ *Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
