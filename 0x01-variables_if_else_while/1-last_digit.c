be99236eb67b527c492aa4784c16fec4bda7eaf1
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-11 21:09:25 +0000

    Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *Description: a C program that checks if random number is +ve or -ve or 0
+ *
+ *Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int n, d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d = n % 10;
+	if (d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, d);
+	}
+	else if (d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, d);
+	}
+	else if (d < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+	}
+	return (0);
+}
