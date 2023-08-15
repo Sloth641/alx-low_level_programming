dc26f200d4e1bae9e2d85157bc6c15d49354b035
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-15 22:39:54 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *Description: a C program that return 0
+ *
+ *Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
