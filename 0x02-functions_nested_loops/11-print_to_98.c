10f71079b4eed9bc4f0c60ff7ccfb9fc55fda084
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-16 02:04:04 +0000

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - prints up(down)wards to 98
+ *
+ * @n: start point
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if(n > 98)
+	{
+		for(i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}	
+	}
+	else
+	{
+		for(i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98\n");
+}
+
