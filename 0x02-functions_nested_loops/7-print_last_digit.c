68a36c6b53484b02afbdbb15f4183127141a2d05
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-16 00:35:23 +0000

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_last_digit - gets last digit of given number and prints it
+ *
+ *@n: input number for fn
+ *
+ * Return: returns last digit.
+ *
+*/
+int print_last_digit(int n)
+{
+	int d;
+
+	if (n < 0)
+	{
+		d = (-1) * (n % 10);
+	}
+	else
+	{
+		d = (n % 10);
+	}
+
+}
