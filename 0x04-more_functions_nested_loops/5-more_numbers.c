60e8d7f8eecd25bc4955cf02cc761a1cb457c3e6
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-17 14:44:16 +0000

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,19 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, temp;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else
+			temp = j;
+			if (j > 9)
 			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
+				_putchar('1');
+				temp = j % 10;
 			}
+			_putchar(temp + '0');
 		}
 		_putchar('\n');
 	}
