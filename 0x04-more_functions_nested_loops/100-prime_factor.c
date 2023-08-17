073fd983944a4c6a14ff1529fc2ad638067df7fd
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-17 17:15:58 +0000

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,21 +10,15 @@
  */
 int main(void)
 {
-	long int n = 1231952;
+	long int n = 612852475143;
 	long int i, maxPrime;
 
-	/* Print the number of 2s that divide n */
 	while (n % 2 == 0)
 	{
 		maxPrime = 2;
 		n = n / 2;
 	}
 
-	/**
-	 * At this point, num must be odd.
-	 * Starting with 3 to square root of num ,
-	 * if divisible, divide and update num, and update CurrMaxPrime
-	 */
 	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
@@ -33,6 +27,7 @@ int main(void)
 			n = n / i;
 		}
 	}
+
 	if (n > 2)
 		maxPrime = n;
 
