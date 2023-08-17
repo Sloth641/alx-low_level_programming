62a65ae49dca21bcaa03b0228fd07fc2940d73ac
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-17 14:08:10 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers.
+ *
+ * @a: input to function
+ * @b: input to function
+ * 
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
