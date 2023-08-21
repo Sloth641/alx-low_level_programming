9b6c9c9642996a02369659b52ed6ccfd0402a927
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-21 23:33:11 +0000

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints 2nd half of a string.
+ *
+ * @str: input string
+ *
+ * Return: Nothing.
+ */
+void puts_half(char *str)
+{
+	int len, n;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
