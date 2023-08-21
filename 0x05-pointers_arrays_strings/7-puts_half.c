1be038f49080fa7011591b68c54d5f8b60163560
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-21 23:46:38 +0000

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,6 +13,7 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 		len++;
+	len++;
 	if (len % 2 == 0)
 	{
 		n = len / 2;
@@ -21,12 +22,10 @@ void puts_half(char *str)
 	{
 		n = (len - 1) / 2;
 	}
-	
 	while (str[n] != '\0')
 	{
 		_putchar(str[n]);
 		n++;
 	}
-	
 	_putchar('\n');
 }
