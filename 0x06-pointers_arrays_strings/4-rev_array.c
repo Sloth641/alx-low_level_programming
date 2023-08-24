1ef2e1a7efcd4afc777c9bca14cf73a37d257b51
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-24 22:28:24 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - hat reverses the content of an array of integers.
+ *
+ * @a: array
+ * @n: number of elements in array.
+ *
+ * Return: Nothing.
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
