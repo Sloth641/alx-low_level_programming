e003e8d42714ab53e84e6c38812f48cd5a44511a
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-25 01:28:53 +0000

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,15 +12,16 @@ char *rot13(char *str)
 {
 	int i, j;
 	char or[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char code[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char co[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; or[j] !='\0'; j++)
+		for (j = 0; j < 53; j++)
 		{
 			if (str[i] == or[j])
 			{
-				str[i] = code[j];
+				str[i] = co[j];
+				break;
 			}
 		}
 	}
