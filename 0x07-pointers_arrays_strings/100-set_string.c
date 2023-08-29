c2653d5b973802f87cc0b0c890d19870f2556b8b
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-29 01:41:40 +0000

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,23 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - searches a string for any of a set of bytes.
+ * set_string - sets the value of a pointer to a char.
  *
- * @a: input rows
- * @size: array dimensions
+ * @s: pointer to pointer to be modified
+ * @to: pointer to copy from
  *
  * Return: nothing.
  */
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int i, j;
-	int leftDiagonalSum = 0, rightDiagonalSum = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		leftDiagonalSum += a[i];
-		rightDiagonalSum += a[size - i - 1];
-		a += size;
-	}
-	printf("%d, %d\n", leftDiagonalSum, rightDiagonalSum);
+	*s = to;
 }
