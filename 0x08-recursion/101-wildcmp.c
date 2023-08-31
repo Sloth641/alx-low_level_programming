45caa572632920c86135b7f2564254428bf973a6
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-08-31 01:52:51 +0000

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,39 +1,40 @@
 #include "main.h"
 
 /**
- * is_match -  compares two strings if the strings can be considered identical.
+ * is_star -  moves past any star we might encounter.
+ *
+ * @s2: input string
+ *
+ * Return: s2 pointer after star.
+ *
+ */
+char *is_star(char *s2)
+{
+	if (*s2 == '*')
+		return (is_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * is_match_cmp -  compares two strings if the strings identical.
  *
  * @s1: input string
  * @s2: input string
- * @index1: index of s1
- * @index2: index of s2
  *
  * Return: 1 if the strings can be considered identical, otherwise 0.
  *
  */
-int is_match(char *s1, char *s2, int index1, int index2)
+int is_match_cmp(char *s1, char *s2)
 {
-	if (s1[index1] == '\0' && s2[index2] == '\0')
-	{
-		return (1);
-	}
-	else if (s2[index2] == '*')
-	{
-		for (; s1[index1] != '\0'; index1++)
-		{
-			if (is_match(s1, s2, index1, index2 + 1))
-				return (1);
-		}
-		return (is_match(s1, s2, index1, index2 + 1));
-	}
-	else if (s1[index1] != '?' && s1[index1] != s2[index2])
-	{
+	int is_match = 0;
+
+	if (*s1 == 0)
 		return (0);
-	}
-	else
-	{
-		return (is_match(s1, s2, index1 + 1, index2 + 1));
-	}
+	if (*s1 == *s2)
+		is_match += wildcmp(s1 + 1, s2 + 1);
+	is_match += is_match_cmp(s1 + 1, s2);
+	return (is_match);
 }
 
 /**
@@ -47,5 +48,27 @@ int is_match(char *s1, char *s2, int index1, int index2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (is_match(s1, s2, 0, 0));
+	int is_match = 0;
+
+	if (!*s1 && *s2 == '*' && !*is_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp((s1 + 1), (*s2 == '*' ? s2 : s2 + 1)));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = is_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			is_match += wildcmp(s1 + 1, s2 + 1);
+		is_match += is_match_cmp(s1, s2);
+		return (!!is_match);
+	}
+	return (0);
 }
