2e42e25f42bb96ec3cc904048820501cfe339e78
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-01 23:18:11 +0000

    Task 0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,19 +13,21 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 	char *c = 0;
 
-	if (argc <= 2)
+	if (argc == 1)
 		printf("%d\n", 0);
 	else
 	{
 		for (i = 1; i < argc; i++)
 		{
 			c = argv[i];
-			if (*c >= '0' && *c <= '9')
-				sum += atoi(c);
-			else
+			if (*c < '0' || *c > '9')
 			{
 				printf("Error\n");
-				return (1);
+                                return (1);
+			}
+			else
+			{
+				sum += atoi(c);
 			}
 		}
 		printf("%d\n", sum);
