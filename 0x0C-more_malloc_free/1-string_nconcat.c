c822944f74d3729e82cfae44fa83c3f61be30882
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-07 15:27:38 +0000

    Task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
-	ptr = malloc(sizeof(char) * (size1 + size2) + 1);
+	ptr = malloc(sizeof(char) * (size1 + n) + 1);
 	if (ptr == NULL)
 	{
 		return (NULL);
