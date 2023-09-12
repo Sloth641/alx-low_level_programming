bbb0bfbb76a5766d13309dfd7f5ca0daa9314687
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-12 00:05:14 +0000

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-   char *name;
-   float age;
-   char *owner;
+/**
+ * struct dog - doggy dog
+ *
+ * @name: param 1
+ * @age: param 2
+ * @owner: param 3
+ *
+ * Description: just a fury dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
