21f385db647346d552003ce6963eb8a9f40ea1e5
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-12 00:30:22 +0000

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,14 +2,19 @@
 /**
  * init_dog - init dog struct
  *
+ * @d: pointer to dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
  * Return: Nothing.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d -> name = name;
-		d -> age = age;
-		d -> owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
