c8c64f1f1b48c883a62237796f9acd2fc298e0ac
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-14 02:24:17 +0000

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+/**
+ * main - Entry Point
+ *
+ * @argc: no. of args
+ * @argv: array of args
+ *
+ * Return: Always 0 (Sucess)
+*/
+int main(int argc, char *argv[])
+{
+	int c, i;
+	char *ptr = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	c = atoi(argv[1]);
+	if (c < 1)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while(c--)
+	{
+		printf("%02hhx%s", *ptr, c ? " " : "\n");
+		ptr++;
+	}
+	return (0);
+}
