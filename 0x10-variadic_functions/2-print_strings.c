7c7f7630695e73563b5bb405d26c66a90612f0a0
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-18 20:48:31 +0000

    Task 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,21 +8,20 @@
  *
  * Return: nothing.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+oid print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list param;
 
 	if (n <= 0)
 	{
-		printf("\n");
 		return;
 	}
 	va_start(param, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(param, int));
-		if ((separator != NULL) && (i != (n - 1)))
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
