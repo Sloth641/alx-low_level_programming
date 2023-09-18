e30ef3743184b64ab3816901e3a0d33cd3233faa
Ahmed Fawzy <ahmedmf251@gmail.com>
2023-09-18 20:59:05 +0000

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(param, char *);
-		if(str == NULL)
+		if (str == NULL)
 			printf("(nil)");
 		else
 			printf("%s", str);
