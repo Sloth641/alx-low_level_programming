24eb16348a826b02db5c813a1eaa6520d5e986f2
Sloth641 <ahmedmf251@gmail.com>
2023-09-26 21:44:41 +0000

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * free_list - frees lists
+ *
+ * @head: pointer to list node
+ *
+ * Return: nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *c = head, *t;
+
+	if (c == NULL)
+	{
+		return;
+	}
+	while (c != NULL)
+	{
+		t = c->next;
+		free(c->str);
+		free(c);
+		c = t; 
+	}
+	head = NULL;
+	return;
+}
