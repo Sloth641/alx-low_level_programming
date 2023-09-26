fc9601a1529ebfee5f3f79dd8e8a548da0426812
Sloth641 <ahmedmf251@gmail.com>
2023-09-26 20:36:13 +0000

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of lists
+ *
+ * @h: pointer to list node
+ *
+ * Return: no. of elements of list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	const list_t *c = h;
+
+	while (c != NULL)
+	{
+		if (c->str == NULL)
+		{
+			printf ("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", c->len, c->str);
+		}
+		c = c->next;
+		n++;
+	}
+	return (n);
+}
