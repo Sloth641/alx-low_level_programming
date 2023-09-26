cf1db74c44901c0e2b5ff89f1739eb7b2f38fecb
Sloth641 <ahmedmf251@gmail.com>
2023-09-26 23:56:25 +0000

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	c = head;
 	for (i = 0; i < index; i++)
 	{
-		c = c->next;
+		if (c != NULL)
+		{
+			c = c->next;
+		}
 	}
 	return (c);
 }
