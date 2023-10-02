b35a299ba00bd2466b646f4a61379a95ed907453
Sloth641 <fawzy@DESKTOP-4G9HOQ5>
2023-10-03 00:32:50 +0300

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX STDOUT.
+ *
+ * @filename: pointer to file
+ * @letters: number of letters it should read and print
+ *
+ * Return:  actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int ff;
+	ssize_t n;
+	char text[1000];
+
+	if (filename == NULL || letters <= 0)
+	{
+		return (0);
+	}
+	ff = open(filename, O_RDONLY);
+	if (ff == -1)
+	{
+		return (0);
+	}
+	n = read(ff, (void *)text, letters);
+	n = write(STDOUT_FILENO, (void *)text, n);
+	close(ff);
+	return (n);
+}
