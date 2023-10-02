f682dc07c0e165c38bdddc889ebf4b29fa0e75df
Sloth641 <fawzy@DESKTOP-4G9HOQ5>
2023-10-03 00:42:29 +0300

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t n;
 	char text[1000];
 
-	if (filename == NULL || letters <= 0)
+	if (filename == NULL || !letters)
 	{
 		return (0);
 	}
