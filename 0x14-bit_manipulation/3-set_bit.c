05c857e440d3f08b1cdd9dbebbde09f645dd4a84
Sloth641 <Ahmedmf251@gmail.com>
2023-12-15 23:13:21 +0200

    Task 0

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
+ * set_bit - Function sets the value of a bit to 1 at a given index.
  *
  * @n: input decimal number
  * @index: no. bit required
